4b7c628ee39032e92f414b6c8255e465698dc86a
Gai Shaked <shaked.gai@gmail.com>
2010-11-25 00:26:26 +0200

    Real (yet very poor) boa skeleton

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <set>
+#include <string>
+#include "constraint.h"
+
+using std::set;
+using std::string;
 
 int main(int argc, char* argv[]) {
-  std::cout << "Buffer overruns are possible!" << std::endl;
-  return 1; // non zero return value means fail => buffer overruns are possible
+  ConstraintProblem cp;
+  // call pointer analysis
+  // ...
+
+  // generage constraints
+  // ...
+
+  // solve constraint problem
+  set<string> unsafeBuffers = cp.Solve();
+  
+  if (unsafeBuffers.empty()) {
+	  std::cout << "Safe!" << std::endl;
+  	return 0;
+  }
+  else {
+	  std::cout << "Buffer overruns are possible!" << std::endl;
+  	return 1; // non zero return value means fail => buffer overruns are possible
+  }
 }
 
