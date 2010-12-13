e7adc936e106a8518d0f25d556c723570899cfc8
Tzafrir Rehan <tzafrir.r@gmail.com>
2010-12-13 13:19:51 +0200

    Add interface method getUniqueName (virtual to allow it to be overriden by tests)

--- source/buffer.h
+++ source/buffer.h
@@ -1,8 +1,14 @@
 #ifndef __BOA_BUFFER_H
 #define __BOA_BUFFER_H /* */
 
+#include <string>
+
+using std::string;
+
 namespace boa {
 	class Buffer {
+	 public:
+	  virtual string getUniqueName();
 	};
 }
 
