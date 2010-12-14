0fe96186d2e280da1f388aec781e3de27d971d17
Gai Shaked <shaked.gai@gmail.com>
2010-12-14 12:39:05 +0200

    tabs => spaces

--- source/PointerAnalysis.cpp
+++ source/PointerAnalysis.cpp
@@ -23,53 +23,53 @@ namespace {
 
 class PointerASTVisitor : public RecursiveASTVisitor<PointerASTVisitor> {
 private:
-	list<Buffer> Buffers_;
+  list<Buffer> Buffers_;
 public:
   bool VisitStmt(Stmt* S) {
     if (DeclStmt* dec = dyn_cast<DeclStmt>(S)) {
       for (DeclGroupRef::iterator i = dec->decl_begin(), end = dec->decl_end(); i != end; ++i) {
-				MyVisitDeclStmt(*i);
+        MyVisitDeclStmt(*i);
       }
     }
     return true;
   }
-  
+
   void MyVisitDeclStmt(Decl *d) {
-		if (VarDecl* var = dyn_cast<VarDecl>(d)) {
+    if (VarDecl* var = dyn_cast<VarDecl>(d)) {
       // FIXME - This code only detects an array of chars
       // Array of array of chars will probably NOT detected
-      // As well as "array of MyChar" When using "typedef MyChar char". 
+      // As well as "array of MyChar" When using "typedef MyChar char".
       if (ArrayType* arr = dyn_cast<ArrayType>(var->getType().getTypePtr())) {
-      	if (BuiltinType* arrType = dyn_cast<BuiltinType>(arr->getElementType())) {
-      		switch (arrType->getKind()) {
-      			case BuiltinType::Char_U: case BuiltinType::UChar: case BuiltinType::Char16: 
-      			case BuiltinType::Char32: case BuiltinType::Char_S: case BuiltinType::SChar: 
-      			case BuiltinType::WChar:
-							addBufferToSet(var);
-      				break;
-      			default: 
-      				// Not a char array
-      				break;
-      		}
-      	}
+        if (BuiltinType* arrType = dyn_cast<BuiltinType>(arr->getElementType())) {
+          switch (arrType->getKind()) {
+            case BuiltinType::Char_U: case BuiltinType::UChar: case BuiltinType::Char16:
+            case BuiltinType::Char32: case BuiltinType::Char_S: case BuiltinType::SChar:
+            case BuiltinType::WChar:
+              addBufferToSet(var);
+              break;
+            default:
+              // Not a char array
+              break;
+          }
+        }
       }
-		}  
+    }
   }
-  
+
   void addBufferToSet(VarDecl* var) {
-  	var->dump();
-  	cerr << " was added to buffers set" << endl;
-  	
-  	Buffer b;
-  	cerr << " code name  = " << var->getNameAsString() << endl;
-		cerr << " \"clang ID\" = " << (void*)var << endl;
-  	// TODO - get var's code location (Tzafrir?)
-  	Buffers_.push_back(b);
+    var->dump();
+    cerr << " was added to buffers set" << endl;
+
+    Buffer b;
+    cerr << " code name  = " << var->getNameAsString() << endl;
+    cerr << " \"clang ID\" = " << (void*)var << endl;
+    // TODO - get var's code location (Tzafrir?)
+    Buffers_.push_back(b);
   }
 };
 
 class PointerAnalysisConsumer : public ASTConsumer {
-public: 
+public:
   virtual void HandleTopLevelDecl(DeclGroupRef DG) {
     for (DeclGroupRef::iterator i = DG.begin(), e = DG.end(); i != e; ++i) {
       Decl *D = *i;
@@ -87,8 +87,8 @@ protected:
   }
   bool ParseArgs(const CompilerInstance &CI,
                  const std::vector<std::string>& args) {
-		return true;
-	}    
+    return true;
+  }
 };
 
 }
