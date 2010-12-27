1bf266bf7182cb3986e29f9cff0187ef8e312ff6
Tzafrir Rehan <tzafrir.r@gmail.com>
2010-12-27 17:52:21 +0200

    Count passed tests, report numbers

--- tests/testAll.sh
+++ tests/testAll.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+NO_COLOR="\033[0m"
+
+tested=0
+passed=0
+
+if [ "$1" == "-v" ]; then
+  verbose=true
+fi
+
 function run_testcase {
+  (( tested++ ))
   tests/testRunner.py $1
-  # TODO(tzafrir): Count tests, failed, run, colors...
+  return $?
 }
 
 for file in $(ls tests/testcases/*.c); do
   TESTNAME=`echo $file | cut -d"." -f1`
   run_testcase $TESTNAME
+  if [ "$?" == "0" ]; then
+    (( passed++ ))
+    if [ $verbose ]; then
+      echo -e "$GREEN""Test Passed: $TESTNAME""$NO_COLOR"
+    fi
+  else
+    echo -e "$RED""Test Failed: $TESTNAME""$NO_COLOR"
+  fi
 done
 
+echo "---"
+echo -e "All Tests:"
+echo -e "  $tested tests run"
+echo -e "  $GREEN$passed tests passed$NO_COLOR"
+if [ "$tested" != "$passed" ]; then
+  echo -e "  $RED$(( $tested - $passed )) tests failed"
+fi
