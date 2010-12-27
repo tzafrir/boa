bb4ae94eaab22bce5d948f9df4733296b524e3cb
Gai Shaked <shaked.gai@gmail.com>
2010-12-28 00:24:36 +0200

    integer test

---
+++ tests/testcases/integer02.c
@@ -0,0 +1,15 @@
+int main() {
+  char buf1[10], buf2[10], buf3[10], buf4[10], buf5[10];
+  int i = 3, j = 5, k, l = 0;
+  char c;
+  buf1[i] = c;
+  buf1[i + j] = c;
+  buf1[2 * i - j] = c;
+  buf1[4 + j] = c;
+  buf2[i - j] = c; // write before the begining of buffer
+  buf3[k] = c; // k uninitialized
+  k = 1;
+  l = j + 5;
+  buf4[l] = c; // l == 10
+}
+
