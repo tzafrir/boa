04da8570fc18941cadd2edc0eb0c57b3be8454bc
Tzafrir Rehan <tzafrir.r@gmail.com>
2011-01-03 13:09:49 +0200

    Fix bug

--- tests/testcases/loop1.c
+++ tests/testcases/loop1.c
@@ -1,8 +1,13 @@
-int i;
-char str[6];
-str[0] = 'a';
-str[1] = 'b';
-str[2] = '\0';
-for (i = 0; i < strlen(str); i++) {
-  str[i] = '9'; 
+#include <string.h>
+
+int main() {
+  int i;
+  char str[6];
+  str[0] = 'a';
+  str[1] = 'b';
+  str[2] = '\0';
+  for (i = 0; i < strlen(str); i++) {
+    str[i] = '9'; 
+  }
+  return 0;
 }
