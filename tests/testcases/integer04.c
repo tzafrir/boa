2782228479e9f580c4468c775832d403cc2ad137
Gai Shaked <shaked.gai@gmail.com>
2011-01-05 18:18:33 +0200

    integer04 tests divide

---
+++ tests/testcases/integer04.c
@@ -0,0 +1,10 @@
+int main() {
+  int i = 2 / 2; // = 1
+  int j = 2 / 3; // = 0
+  int k = 20 / 5; // = 4
+  char buf1[2], buf2[1], buf3[4];
+  buf1[i] = 'a';
+  buf2[j] = 'b';
+  buf3[k] = 'c';
+  return 0;
+}
