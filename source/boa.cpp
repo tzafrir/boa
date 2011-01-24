2ff3ed2fec55dc7e94919dfff8e76fe76a8e979f
Tzafrir Rehan <tzafrir.r@gmail.com>
2011-01-24 14:30:35 +0200

    Pass bitcode between modules, ignore bitcode output, log to stderr, whitespace

--- source/boa.cpp
+++ source/boa.cpp
@@ -26,20 +26,20 @@ class boa : public ModulePass {
  private:
   ConstraintProblem constraintProblem_;
   ConstraintGenerator constraintGenerator_;
-  
+
  public:
   static char ID;
-  
+
 
   boa() : ModulePass(ID), constraintGenerator_(constraintProblem_) {
-    log::set(std::cout); // TODO - print log only when neseccery
+    log::set(cerr); // TODO - print log only when neseccery
    }
 
   virtual bool runOnModule(Module &M) {
     for (Module::const_iterator it = M.begin(); it != M.end(); ++it) {
-      const Function *F = it; 
+      const Function *F = it;
       for (const_inst_iterator ii = inst_begin(F); ii != inst_end(F); ++ii) {
-        constraintGenerator_.VisitInstruction(&(*ii));           
+        constraintGenerator_.VisitInstruction(&(*ii));
       }
     }
     return false;
@@ -146,8 +146,8 @@ class boa : public ModulePass {
       cerr << SEPARATOR << endl;
     }
   }
-  
-  
+
+
 };
 }
 
