4a8a0ecaac496c52188246457a7e357b9d0a40bf
Tzafrir Rehan <tzafrir.r@gmail.com>
2011-02-02 14:36:15 +0200

    Style changes

--- source/log.cpp
+++ source/log.cpp
@@ -3,18 +3,21 @@
 using std::ostream;
 
 namespace boa {
-  namespace log {
-    class nullstream : public ostream {
-      template <class T> const nullstream& operator<<(const T& m) const {return *this;}
-    } devNull;
 
-    ostream *os_ = &devNull;
+namespace log {
+  class nullstream : public ostream {
+    template <class T> const nullstream& operator<<(const T& m) const {return *this;}
+  } devNull;
 
-    void set(ostream &os) {
-      os_=&os;
-    }
-    ostream &os() {
-      return *os_;
-    }
+  ostream *os_ = &devNull;
+
+  void set(ostream &os) {
+    os_=&os;
+  }
+
+  ostream &os() {
+    return *os_;
   }
 }
+
+}  // namespace boa
