229441bf4b96f3dc9b9acf65d6add8fbb02ed719
Tzafrir Rehan <tzafrir.r@gmail.com>
2011-02-19 20:24:31 +0200

    Add check for safe access on phi and select

--- tests/testcases/phi1.c
+++ tests/testcases/phi1.c
@@ -4,7 +4,10 @@ int main() {
   // Ternary operator with an expression is compiled into a phi node.
   int k = i < 6 ? c-1 : 14;
   int l = j < 6 ? 12 : c+1;
-  char buf1[13], buf2[13];
+  char buf1[13], buf2[13], buf3[13];
   buf1[k] = 't';
   buf2[l] = 'z';
+  int m = 5;
+  int n = m < 6 ? c-5 : c-6;
+  buf3[n] = 'a';
 }
--- tests/testcases/select1.c
+++ tests/testcases/select1.c
@@ -4,7 +4,9 @@ int main() {
   // Ternary operator with constants compiles into a select node.
   int k = i < 6 ? 12 : 14;
   int l = j < 6 ? 12 : 14;
-  char buf1[13], buf2[13];
+  int m = j < 6 ? 10 : 11;
+  char buf1[13], buf2[13], buf3[13];
   buf1[k] = 't';
   buf2[l] = 'z';
+  buf3[m] = 'a';
 }
