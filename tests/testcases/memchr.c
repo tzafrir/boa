75049e28c34cad822189d7c0db1a5c81630d882c
Tzafrir Rehan <tzafrir.r@gmail.com>
2011-02-27 21:08:52 +0200

    Tests for memchr

---
+++ tests/testcases/memchr.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+int main() {
+  char unsafe[4] = "abc";
+  char safe[] = "Safe string";
+  char safeAlias[10] = "123456789";
+  char unsafeAlias[4] = "abc";
+  char* c = memchr(unsafe, 'b', 10);
+  if (c != NULL) return 0;
+  c = memchr(safe, 'z', 2);
+  char* a1 = memchr(safeAlias, '1', 1);
+  a1[3] = '0';
+  char* a2 = memchr(unsafeAlias, 'd', 5);
+  a2[0] = '0';
+  return 0;
+}
