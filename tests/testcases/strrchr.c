cc4b9928f5958c56230278b4a10070784c74d3f3
Gai Shaked <shaked.gai@gmail.com>
2011-02-28 10:21:59 +0200

    tests

---
+++ tests/testcases/strrchr.c
@@ -0,0 +1,9 @@
+#include "string.h"
+
+int main() {
+  char buf1[] = "hello", buf2[] = "world";
+  char *p1 = strrchr(buf1, 'o');
+  char *p2 = strrchr(buf2, '\0');
+  p1[0] = 'a';
+  p2[1] = 'a';
+}
